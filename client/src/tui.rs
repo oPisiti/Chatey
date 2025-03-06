63a3d7160c174896d4c170a3e0ea6942eb30349d
leah <laurohrfranca@gmail.com>
2025-03-06 17:34:29 -0300

    Input box now spans the whole screen width

--- client/src/tui.rs
+++ client/src/tui.rs
@@ -1,4 +1,4 @@
-use std::{io::Error, iter::once, sync::Arc};
+use std::{io::Error, sync::Arc};
 
 use crossterm::event::{self, Event, KeyCode, KeyModifiers};
 use futures_util::StreamExt;
@@ -31,6 +31,21 @@ pub async fn run(
     let mut input_box = Vec::new();
     let mut keyboard_reader = event::EventStream::new();
     let mut terminal = terminal;
+
+    // Create layouts
+    let mag_input_layout = Layout::vertical([
+        Constraint::Percentage(90),
+        Constraint::Fill(1),
+    ]);
+    let msg_vertical_layout = Layout::vertical(
+        [Constraint::Ratio(1, MAX_MESSAGES_ON_SCREEN.into());
+        MAX_MESSAGES_ON_SCREEN as usize],
+    );
+    let msg_horizontal_layout = Layout::horizontal([
+        Constraint::Percentage(40),
+        Constraint::Fill(1),
+        Constraint::Percentage(40),
+    ]);
 
     loop {
         // Create outer block
@@ -70,40 +85,33 @@ pub async fn run(
 
         // Draw a frame
         let draw_result = terminal.draw(|frame| {
+            // --- Creating areas ---
+            // Outer frame
             let outer = frame.area();
 
-            // Create layouts
-            let vertical = Layout::vertical(
-                [Constraint::Ratio(1, (MAX_MESSAGES_ON_SCREEN + 1).into());
-                (MAX_MESSAGES_ON_SCREEN + 1) as usize],
-            );
-            let horizontal = Layout::horizontal([
-                Constraint::Percentage(40),
-                Constraint::Fill(1),
-                Constraint::Percentage(40),
-            ]);
+            // Devide outer into a messages box and an input box
+            let [msg_box, input_area] = mag_input_layout.areas(outer.inner(Margin::new(1, 1)));
 
-            // Creating areas
-            let vertical_areas: [Rect; (MAX_MESSAGES_ON_SCREEN + 1) as usize] =
-                vertical.areas(outer.inner(Margin::new(1, 1)));
+            // Devide the messages box into vertival parts
+            let vertical_areas: [Rect; MAX_MESSAGES_ON_SCREEN as usize] =
+                msg_vertical_layout.areas(msg_box);
 
             // The final message areas are an array of [left, mid, right] areas
             // Each message area is meant to be used with a single of the three areas
             let mut msg_areas: Vec<[Rect; 3]> = Vec::new();
             for v in vertical_areas.iter().rev() {
-                msg_areas.push(horizontal.areas(*v));
+                msg_areas.push(msg_horizontal_layout.areas(*v));
             }
 
             // Draw each widget
             frame.render_widget(&outer_block, outer);
-
-            let blocks_iterator = once(&input_block).chain(msg_blocks.iter());
-            for (i, msg) in blocks_iterator.enumerate() {
+            frame.render_widget(input_block, input_area);
+            for (i, msg) in msg_blocks.iter().enumerate() {
                 frame.render_widget(msg, msg_areas[i][0]);
             }
         });
 
-        // TODO: deal with draw result
+        // Deal with draw result
         if draw_result.is_err() {
             log::error!("Failed to render frame: {}", draw_result.unwrap_err());
         }
@@ -113,7 +121,6 @@ pub async fn run(
             // Wait for a change in history notification via "notify_rx"
             _ = notify_rx.recv() => continue,
 
-            // TODO: Properly deal with all possibillities
             // Wait for a key to be pressed
             keyboard_event = keyboard_reader.next() => match keyboard_event{
                 Some(Ok(event)) => match event {
